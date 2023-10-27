79bae4559d7471d389b199a67aa7ad23fc1c5a77
benaichacodes <deve90codes@yahoo.com>
2023-10-27 20:11:49 +0200

    Home Page Login

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,14 +1,48 @@
 <?php
   include_once("inc/header.php"); ?>
- <main class="container">
-      <div class="bg-body-tertiary p-5 rounded">
-        <h1>Navbar example</h1>
-        <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
-        <a class="btn btn-lg btn-primary" href="/docs/5.3/components/navbar/" role="button">View navbar docs &raquo;</a>
+       <main role="main">
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="container">
+    <div class="jumbotron" style="margin-top: 5%;">
+      <div class="container">
+        <h1 class="display-3">Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
       </div>
-      <div class="col-md-5">
-         
-      </div>
-      </div>
-      <?php include_once("inc/footer.php");?>
-</main>
+    </div>
+
+  <!-- Example row of columns -->
+  <div class="row">
+        <div class="col-md-10">
+          <h2>Heading</h2>
+          <p>Will you do the same for me? It's time to face the music I'm no longer your muse. Heard it's beautiful, be the judge and my girls gonna take a vote. I can feel a phoenix inside of me. Heaven is jealous of our love, angels are crying from up above. Yeah, you take me to utopia.</p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-2">
+          <p><a class="btn btn-secondary" href="#" role="button">View &raquo;</a></p>
+        </div>
+        
+        <div class="col-md-10">
+          <h2>Heading</h2>
+          <p>Will you do the same for me? It's time to face the music I'm no longer your muse. Heard it's beautiful, be the judge and my girls gonna take a vote. I can feel a phoenix inside of me. Heaven is jealous of our love, angels are crying from up above. Yeah, you take me to utopia.</p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-2">
+          <p><a class="btn btn-secondary" href="#" role="button">View &raquo;</a></p>
+        </div>
+        
+        <div class="col-md-10">
+          <h2>Heading</h2>
+          <p>Will you do the same for me? It's time to face the music I'm no longer your muse. Heard it's beautiful, be the judge and my girls gonna take a vote. I can feel a phoenix inside of me. Heaven is jealous of our love, angels are crying from up above. Yeah, you take me to utopia.</p>
+          <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-2">
+          <p><a class="btn btn-secondary" href="#" role="button">View &raquo;</a></p>
+        </div>
+  </div>
+
+  <hr>
+
+  <?php include_once("inc/footer.php");?>
+</div> <!-- /container -->
