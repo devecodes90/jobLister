a0d5c3eb1449bacd2d362745f2001725f798f372
benaichacodes <deve90codes@yahoo.com>
2023-10-27 23:28:09 +0200

    Configuration database

--- config/__config.php
+++ config/__config.php
@@ -1,5 +1,9 @@
 <?php 
+// DB PARAMS
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'php_joblister');
 
-
-
+define('SITE_TITLE', 'JobLister');
 ?>
