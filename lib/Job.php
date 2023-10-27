51be4171afda62e89fb8969703cbf5cb94e62b66
benaichacodes <deve90codes@yahoo.com>
2023-10-27 23:44:08 +0200

    Fetch All Jobs from the database and return a list of all the jobs

--- lib/Job.php
+++ lib/Job.php
@@ -1,11 +1,20 @@
 <?php 
 
-
-
-
-
-
-
+ class Job
+{
+    private $db;
+    
+    public function __construct( $db ){
+      $this->db = new Database();
+    }
+    
+    // Get all the jobs
+    public function getAllJobs(){
+       $this->db->query("SELECT jobs.*, categories.name AS cname FROM jobs INNER JOIN categories ON jobs.category_id = categories.id ORDER BY post_date DESC");
+       $results = $this->db->resultSet() ;  
+       return $results;
+    }
+}
 
 
 ?>
